595e8316f2fa39cdb2186222942a6000337cd8dd
Bhuvneshwari Patil <bhuvneshwaripatil250@gmail.com>
2026-06-11 17:27:24 +0530

    t-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Job_Application_Form_2';
+   @ViewChild('jobForm') jobForm !: NgForm;
+  onSubmit(){
+    if(this.jobForm.invalid){
+      this.jobForm.control.markAllAsTouched()
+    }else{
+    this.jobForm.resetForm();
+    }
+  }
 }
